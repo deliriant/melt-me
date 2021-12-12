895dc2150ae22d119b4ebd0090871021b1b308c2
deliriant <83244375+deliriant@users.noreply.github.com>
2021-12-12 13:52:21 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home() {
       <div className="flex flex-col items-center min-h-screen mx-6">
         <Toaster />
         <div className="flex items-center justify-between w-full mt-3">
-          <h1 className="text-2xl font-bold">next-candy-machine</h1>
+          <h1 className="text-2xl font-bold">Milk me!</h1>
           <div className="flex items-center">
             {connected && (
               <div className="flex items-end mr-2">
